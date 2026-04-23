448927f6d16e9439bda5431236fd41b03a58c3c3
Conor Hamill <conor.hamill@hotmail.com>
2026-04-23 21:22:05 +0100

    Add Bayesian estimator module

---
+++ testing-area/Bayesian_Estimator/test_script.m
@@ -0,0 +1,5 @@
+% this script will contain implementation of a bayesian estimation 
+% taking the nonlinear y output function - euclidian distance 
+% from the 3 beacons with noise v. and constraints, what is the estimate
+
+% TBC
